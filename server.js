3c60700906398c7495db4375b67549eb434e4eea
Kawfang3025 <5930213025@email.psu.ac.th>
2018-10-01 16:15:26 +0700

    edit port setting

--- server.js
+++ server.js
@@ -31,31 +31,31 @@ app.get('/products', function (req, res) {
     var sql = 'select * from products';
 
     if (id) {
-            sql +=' where id ='+id;
+        sql += ' where id =' + id;
     }
-        db.any(sql)
-            .then(function (data) {
-                console.log('DATA:' + data);
-                res.render('pages/products', { products: data });
-            })
-            .catch(function (error) {
-                console.log('ERROR:' + error);
-            })
+    db.any(sql)
+        .then(function (data) {
+            console.log('DATA:' + data);
+            res.render('pages/products', { products: data });
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+        })
 
 });
 
 
-app.get('/products/:pid',function (req,res){
+app.get('/products/:pid', function (req, res) {
     var pid = req.params.pid;
     var sql = "select * from products where id =" + pid;
     db.any(sql)
-            .then(function (data) {
-                res.render('pages/product_edit', { product: data[0]});
-            })
-            .catch(function (error) {
-                console.log('ERROR:' + error);
-            })
-    
+        .then(function (data) {
+            res.render('pages/product_edit', { product: data[0] });
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+        })
+
 });
 
 app.get('/users', function (req, res) {
@@ -74,7 +74,7 @@ app.get('/users/:id', function (req, res) {
     var sql = 'select * from users';
 
     if (id) {
-            sql +=' where id ='+id;
+        sql += ' where id =' + id;
     }
 
     db.any(sql)
@@ -91,14 +91,16 @@ app.get('/users/:id', function (req, res) {
 app.get('/index', function (req, res) {
     res.render('pages/index');
 });
-app.post('/products/update', function (req,res){
+app.post('/products/update', function (req, res) {
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
     var sql = `update product set title =${title},price = ${price} where id = ${id}`;
     // db.none
-    console.log('UPDATE:' +sql);
+    console.log('UPDATE:' + sql);
     res.redirect('/products');
 });
-console.log('App is running at http://localhost:8080');
-app.listen(8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+    console.log('App is running on http://localhost:' + port);
+});
